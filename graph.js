8af1d8549e8514c9827323f1d02a04b903f79a14
owang <hilow0090@gmail.com>
2024-06-20 01:01:26 -0700

    first and last commit

---
+++ graph.js
@@ -0,0 +1,80 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  // this function accepts a Node instance and adds it to the nodes property on the graph
+  addVertex(vertex) {
+    this.nodes.add(vertex)
+  }
+
+  // this function accepts an array of Node instances and adds them to the nodes property on the graph
+  addVertices(vertexArray) {
+    for(let node of vertexArray){
+      this.nodes.add(node)
+    }
+  }
+
+  // this function accepts two vertices and updates their adjacent values to include the other vertex
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2)
+    v2.adjacent.add(v1)
+  }
+
+  // this function accepts two vertices and updates their adjacent values to remove the other vertex
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2)
+    v2.adjacent.delete(v1)
+  }
+
+  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {
+    this.nodes.delete(vertex)
+    for(let node of vertex.adjacent){
+      node.adjacent.delete(vertex)
+    }
+  }
+
+  // this function returns an array of Node values using DFS
+  depthFirstSearch(start) {
+    function DFS(vert, visited = new Set(), answer = []) {
+      visited.add(vert);
+      answer.push(vert.value);
+
+      for (let node of vert.adjacent) {
+        if (!visited.has(node)) {
+          DFS(node, visited, answer);
+        }
+      }
+      return answer;
+    }
+    return DFS(start);
+
+  }
+
+  // this function returns an array of Node values using BFS
+  breadthFirstSearch(start) {
+    answer = [start.value]
+    seen = new Set([start])
+    queue = [start]
+    while(queue.length>0){
+      current = queue.shift()
+      for (let neighbor of current){
+        if (!(seen.has(neighbor))){
+          seen.add(neighbor)
+          answer.push(neighbor.value)
+          queue.push(neighbor)
+        }
+      }
+    }
+  }
+}
+
+module.exports = {Graph, Node}
